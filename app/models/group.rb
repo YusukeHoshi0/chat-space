6bf6430e376e22e67da7deedcd34d24b32303b6b
YusukeHoshin0 <h1133082@gmail.com>
2019-07-18 22:28:43 +0900

    display member list

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = self.messages.last).present?
+      last_message.body? ? last_message.body : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
