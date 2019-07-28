c2de54bf8786c61aee30f4e455ed90fe5f353253
YusukeHoshin0 <h1133082@gmail.com>
2019-07-28 15:28:48 +0900

    message-asynchronous_communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,82 @@
+$(function() {
+  function buildHTML(message){
+    if (message.body && message.image) {      
+      var html = `<div class="content">
+                    <div class="content__upper-info">
+                      <div class="content__upper-info--user-name">
+                        ${message.name}
+                      </div>
+                      <div class="content__upper-info--date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="content--body">
+                      <p>${message.body}</p>
+                      <img src="${message.image}"></img>
+                    </div>
+                  </div>`
+      return html;
+    }
+    else if (message.body){
+      var html = `<div class="content">
+                    <div class="content__upper-info">
+                      <div class="content__upper-info--user-name">
+                        ${message.name}
+                      </div>
+                      <div class="content__upper-info--date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="content--body">
+                      <p>${message.body}</p>
+                    </div>
+                  </div>`
+      return html;
+    }
+    else if (message.image){
+      var html = `<div class="content">
+                    <div class="content__upper-info">
+                      <div class="content__upper-info--user-name">
+                        ${message.name}
+                      </div>
+                      <div class="content__upper-info--date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="content--body">
+                      <img src="${message.image}"></img>
+                    </div>
+                  </div>`
+      return html;
+    }
+  }
+
+  
+  
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formdata = new FormData(this);
+    var url = $(this).attr('action');
+
+    $('.submit-btn').removeAttr('data-disable-with');
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formdata,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.contents').append(html);
+      $('.input-box__text').val('');
+      $('.input-box__image__file').val('');
+      $('.contents').animate({scrollTop: $('.contents')[0].scrollHeight });
+    })
+    .fail(function(){
+      alert('メッセージを入力して下さい');
+    })
+  })
+})
