b81c7f35ada5cb5fb9e808ed668033046f393618
YusukeHoshin0 <h1133082@gmail.com>
2019-07-28 16:04:49 +0900

    change timezone to Tokyo

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,14 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    config.i18n.default_locale = :ja
-      config.generators do |g|
+    config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
     end
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
   end
 end
